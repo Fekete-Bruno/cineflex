1c17be6bcefa17b24c5330ff6f668e667241d5e2
Fekete-Bruno <brunohffekete@gmail.com>
2022-07-26 14:31:57 -0300

    Added css files and started Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainScreen from "./MainScreen"
+import Header from "./Header/Header";
 import "../css/reset.css"
+import "../css/style.css"
+
 
 export default function App(){
     return(
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<MainScreen />} />
             </Routes>
