e27656226549b686b1f94d3b79872675de020e7e
Fekete-Bruno <brunohffekete@gmail.com>
2022-07-28 22:21:14 -0300

    Seats are shown  and selected

--- src/components/Seats/Seats.js
+++ src/components/Seats/Seats.js
@@ -8,6 +8,7 @@ export default function Seats(){
     const {sessionId} = useParams();
     const apiURL= `https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${sessionId}/seats`
     const [seatsInfo, setSeatsInfo] = useState(null);
+    const [selectedSeats,setSelectedSeats] = useState([]);
     useEffect(()=>{
         const promise = axios.get(apiURL);
         promise.then((response)=>{
@@ -15,16 +16,41 @@ export default function Seats(){
         })
     },[])
 
+    function testedeseats(){
+        const newArray = seatsInfo.seats.filter((seat)=>{return (seat.clicked)}).map((seat)=>{return(Number(seat.name))});
+        setSelectedSeats([...newArray]);
+    }
+
     return(
         <div>
         <InnerHeader text={"Selecione o(s) assento(s)"} />
         {(!seatsInfo)?(<div>Carregando...</div>):(
             <>
             <div className="seats">{
-                seatsInfo.seats.map((seat)=>{return(
-                <Seat key={seat.id} name={seat.name}/>
+                seatsInfo.seats.map((seat,index)=>{return(
+                <Seat key={seat.id} index={index} name={seat.name} available={(seat.isAvailable)?("available"):("unavailable")} seatsInfo={seatsInfo}/>
                 )})}
             </div>
+
+            <div className="examples">
+                <div className="example">
+                <Seat  available={"true"} />
+                Selecionado
+                </div>
+
+                <div className="example">
+                <Seat  available={"available"} />
+                Disponível
+                </div>
+
+                <div className="example">
+                <Seat  available={"unavailable"} />
+                Indisponível
+                </div>
+            </div>
+
+            <div onClick={testedeseats}>BOTAOTESTETETSTE</div>
+
             <Link to="/sucesso"> {/* TEMPORARIOS PARA TESTES DE LAYOUT*/}
             <div className="footer">
                 <div className="img-container"><img src={seatsInfo.movie.posterURL} alt={seatsInfo.movie.title} /></div>
@@ -39,8 +65,19 @@ export default function Seats(){
         </div>
     );
 }
-function Seat({name}){
+function Seat({name , available, index, seatsInfo}){
+    const [clicked,setClicked] = useState(false);
+
+    function testaAssento(){
+        if(available==="available"){
+            setClicked(!clicked);    
+            seatsInfo.seats[index].clicked=!clicked;
+        }else{
+            alert("Esse assento não está disponível...");
+        } 
+    }
+
     return(
-        <div className="seat">{name}</div>
+        <div onClick={testaAssento} className={"seat "+ available + " " + clicked}>{name}</div>
     );
 }
